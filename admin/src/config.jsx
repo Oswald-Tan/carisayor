1cef5171d8491b31b668496603aa64a48a3ecc87
Oswald2809 <oswaldtanlee44@gmail.com>
2025-02-02 01:29:23 +0800

    Update Bug

--- admin/src/config.jsx
+++ admin/src/config.jsx
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/api/v1";
-const API_URL_STATIC = "http://localhost:8080/uploads";
+const API_URL = "https://api.carisayor.co.id/api/v1";
+const API_URL_STATIC = "https://api.carisayor.co.id/uploads";
 
 export { API_URL, API_URL_STATIC };
